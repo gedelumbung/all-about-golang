52e93032d4681881d397db3c65e9bde21209a94c
Gede Lumbung <gedesumawijaya@gmail.com>
2017-03-27 23:41:47 +0800

    go : basic sorting

---
+++ basic/basic_routing.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "sort"
+
+func main() {
+
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
+
+	i := sort.IntsAreSorted(ints)
+	fmt.Println("Integer Sorted: ", i)
+
+	s := sort.StringsAreSorted(strs)
+	fmt.Println("String Sorted: ", s)
+}
